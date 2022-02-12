16d033e11696162ab9d232133b0576c3e121a3bd
Shahn Hogan <shahnhogan@hotmail.com>
2022-02-12 16:40:06 -0600

    switch to parcel and configure

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,57 @@
 module Main exposing (main)
 
 import Accessibility exposing (..)
+import Browser
 import Html.Attributes exposing (class, for, id, name, placeholder)
 
 
+
+-- MAIN
+
+
 main =
-    div [] [ text "hello" ]
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { title : String }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { title = "Tailwind" }, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    main_ [ class "bg-gray-light p-xl" ]
+        [ h1 [] [ text "Welcome" ]
+        ]
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{css,elm,html,js}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#fff',
+      black: '000',
+      'gray-light': '#f9f9f9'
+    },
+    spacing: {
+      xs: '4px',
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '32px',
+      '2xl': '64px',
+      '3xl': '124px'
+    },
+    extend: {}
+  },
+  plugins: []
+}
