4c36fa468d4f3b871ee0495768635baa22d6fcbc
Shahn Hogan <shahnhogan@hotmail.com>
2022-02-12 18:16:42 -0600

    colors, forms, and fun

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (main)
 
 import Accessibility exposing (..)
+import Accessibility.Aria as Aria
 import Browser
-import Html.Attributes exposing (class, for, id, name, placeholder)
+import Html.Attributes exposing (attribute, class, for, id, name, placeholder)
+import Html.Events exposing (onInput)
 
 
 
@@ -23,12 +25,18 @@ main =
 
 
 type alias Model =
-    { title : String }
+    { title : String
+    , passcode : String
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { title = "Tailwind" }, Cmd.none )
+    ( { title = "Verify Passcode"
+      , passcode = ""
+      }
+    , Cmd.none
+    )
 
 
 
@@ -37,6 +45,7 @@ init _ =
 
 type Msg
     = NoOp
+    | Input String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -45,13 +54,37 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
+        Input value ->
+            let
+                passcode =
+                    value
+                        |> String.trim
+                        |> String.slice 0 6
+            in
+            ( { model | passcode = passcode }, Cmd.none )
+
 
 
 -- VIEW
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
-    main_ [ class "bg-gray-light p-xl" ]
-        [ h1 [] [ text "Welcome" ]
+    main_ [ class "bg-grey-light p-lg" ]
+        [ h1 [ class "font-bold text-xl mb-md" ] [ text model.title ]
+        , section []
+            [ labelBefore [ class "font-bold m-sm placeholder:text-black placeholder:tracking-widest" ]
+                (text "Enter Your Code")
+                (inputNumber model.passcode
+                    [ class "p-xs block m-sm border-grey-dark border-1 text-xl"
+                    , id "passcode"
+                    , attribute "autocomplete" "one-time-code"
+                    , attribute "autocapitalize" "off"
+                    , attribute "autocorrect" "off"
+                    , attribute "inputmode" "numeric"
+                    , placeholder "000000"
+                    , onInput Input
+                    ]
+                )
+            ]
         ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,32 @@ module.exports = {
       transparent: 'transparent',
       white: '#fff',
       black: '000',
-      'gray-light': '#f9f9f9'
+      grey: {
+        'x-light': '#f9f9f9',
+        light: '#eee',
+        medium: '#ccc',
+        dark: '#666',
+        'x-dark': '#333'
+      },
+      blue: { light: '#ebecfd', medium: '#1c23a5', dark: '#1a1d5e' },
+      green: { light: '#edfaed', '-dark': 'green' },
+      yellow: { light: '#fdf6e5', medium: '#ffc930' },
+      dark: '#eeb207',
+      red: { light: '#fff5f5', dark: '#c00' }
+    },
+    letterSpacing: {
+      wide: '.8em',
+      widest: '1em'
     },
     spacing: {
       xs: '4px',
       sm: '8px',
       md: '16px',
-      lg: '24px',
-      xl: '32px',
-      '2xl': '64px',
-      '3xl': '124px'
+      lg: '32px',
+      xl: '64px',
+      '2xl': '128px'
     },
     extend: {}
   },
-  plugins: []
+  plugins: [require('@tailwindcss/forms')]
 }
